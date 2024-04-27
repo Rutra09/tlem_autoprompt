049b3aafe8aa398451ff7d2e15a519474749f728
Rutra09 <amzadrozny@gmail.com>
2024-04-27 18:44:08 +0200

    AutoUpdating and Code style

--- Tlem_AutoPrompt.user.js
+++ Tlem_AutoPrompt.user.js
@@ -1,30 +1,31 @@
 // ==UserScript==
 // @name         Tlem AutoPrompt
 // @namespace    http://tampermonkey.net/
-// @version      2024-04-27
+// @version      0.1
 // @description  try to take over the world!
 // @author       ArturM
 // @match        https://edu.t-lem.com/
+// @updateURL    https://github.com/Rutra09/tlem_autoprompt/raw/master/Tlem_AutoPrompt.user.js
 // @downloadURL  https://github.com/Rutra09/tlem_autoprompt/raw/master/Tlem_AutoPrompt.user.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=t-lem.com
 // @grant        none
 // ==/UserScript==
 
-let isFoundText= false;
+let isFoundText = false;
 let interval;
 let Gtext;
-   function getAllTextFromElement(element) {
-      let text = "";
-      if (element.nodeType === 3) {
-          text += element.textContent;
-      } else {
-          for (let child of element.childNodes) {
-              text += getAllTextFromElement(child);
-          }
-      }
-      return text;
-   }
+function getAllTextFromElement(element) {
+    let text = "";
+    if (element.nodeType === 3) {
+        text += element.textContent;
+    } else {
+        for (let child of element.childNodes) {
+            text += getAllTextFromElement(child);
+        }
+    }
+    return text;
+}
 
 function copyToClipboard() {
     let textToCopy = document.getElementById('prompt-text');
@@ -36,12 +37,12 @@ function prepareAndShowPrompt(text, fileData) {
     let prompt;
     prompt = "W poniższym cytacie otrzymasz zadanie. Masz odpowiadać samymy gotowym kodem. \n\"";
     prompt += text;
-for (let file of fileData) {
+    for (let file of fileData) {
         prompt += "```" + file.name + "\n" + file.content + "```\n";
     }
     prompt += "\""
-    if(document.getElementById("prompt-container")) {
-                document.getElementById("prompt-container").remove();
+    if (document.getElementById("prompt-container")) {
+        document.getElementById("prompt-container").remove();
     }
     let promptContainer = document.createElement('div');
     promptContainer.id = 'prompt-container';
@@ -62,7 +63,7 @@ for (let file of fileData) {
     let closeButton = document.createElement('button');
     closeButton.id = 'close-button';
     closeButton.textContent = 'X';
-    closeButton.onclick = function() {
+    closeButton.onclick = function () {
         promptContainer.remove();
     };
     let copyButton = document.createElement('button');
@@ -87,58 +88,58 @@ for (let file of fileData) {
 }
 
 function runDaShit() {
-            'use strict';
+    'use strict';
 
-   let el = document.getElementById("lekcja-t_tresc");
+    let el = document.getElementById("lekcja-t_tresc");
     //check for the type of the lesson
 
 
     let text = getAllTextFromElement(el)
     let files = document.querySelectorAll('[id^="tab_file_"]');
-    if(!(text === "")) {
+    if (!(text === "")) {
         clearInterval(interval);
         Gtext = text;
         isFoundText = true;
-          let fileData = [];
+        let fileData = [];
         for (let file of files) {
             document.querySelector("[href='#" + file.id + "']").click();
         }
         setTimeout(() => {
-                for (let file of files) {
-            let fileName = file.id; // Dostosuj do rzeczywistej nazwy pliku
-            let fileContent = "";
-            let codeLayer = file.querySelector('.ace_layer.ace_text-layer');
+            for (let file of files) {
+                let fileName = file.id; // Dostosuj do rzeczywistej nazwy pliku
+                let fileContent = "";
+                let codeLayer = file.querySelector('.ace_layer.ace_text-layer');
 
-            if (codeLayer) {
-                // Iteruj po wierszach kodu
-                let codeLines = codeLayer.querySelectorAll('.ace_line');
-                for (let line of codeLines) {
-                    fileContent += line.textContent + '\n'; // Dodaj koniec linii
+                if (codeLayer) {
+                    // Iteruj po wierszach kodu
+                    let codeLines = codeLayer.querySelectorAll('.ace_line');
+                    for (let line of codeLines) {
+                        fileContent += line.textContent + '\n'; // Dodaj koniec linii
+                    }
                 }
+                fileData.push({ name: fileName, content: fileContent });
             }
-            fileData.push({ name: fileName, content: fileContent });
-        }
-        prepareAndShowPrompt(Gtext, fileData);
+            prepareAndShowPrompt(Gtext, fileData);
         }, 250)
     }
 }
 
-function timeoutedShit(){
-setTimeout(runDaShit, 1000)
+function timeoutedShit() {
+    setTimeout(runDaShit, 1000)
 }
 
 function onTaskReady() {
     'use strict';
     let element = document.getElementById("lekcja-t_tresc");
-    interval = setInterval(runDaShit,100);
-        let nextButton = document.getElementById("lekcja_next");
+    interval = setInterval(runDaShit, 100);
+    let nextButton = document.getElementById("lekcja_next");
     if (nextButton) {
-        nextButton.addEventListener('click',timeoutedShit );
+        nextButton.addEventListener('click', timeoutedShit);
     }
 }
 
 
-(function() {
+(function () {
     'use strict';
-    waitForKeyElements ("#lekcja-t_tresc", onTaskReady,true);
+    waitForKeyElements("#lekcja-t_tresc", onTaskReady, true);
 })();
